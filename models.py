7c116c6107ed1c28bb77f593c72f76e5a070e3a3
Luke Benstead <lukeb@potatolondon.com>
2014-04-22 15:10:20 +0100

    Implement more logging stuff

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ import sys
 import traceback
 import json
 import os
+import logging
 
 from django.utils import timezone
 from django.db import models
@@ -25,7 +26,7 @@ class Error(models.Model):
     file_path = models.CharField(max_length=500)
     line_number = models.PositiveIntegerField()
     is_resolved = models.BooleanField()
-    event_count = models.PositiveIntegerField(default=1)
+    event_count = models.PositiveIntegerField(default=0)
     last_event = models.DateTimeField(auto_now_add=True)
     level = models.CharField(max_length=100, choices=EVENT_LEVEL_CHOICES, default=EVENT_LEVEL_ERROR)
 
@@ -35,19 +36,26 @@ class Error(models.Model):
         ]
 
 class Event(models.Model):
-    error = models.ForeignKey(Error)
+    error = models.ForeignKey(Error, related_name="events")
     created = models.DateTimeField(auto_now_add=True)
     logged_in_user_email = models.EmailField()
 
     request_method = models.CharField(max_length=10)
     request_url = models.TextField()
     request_querystring = models.TextField()
+    request_repr = models.TextField()
+
+    html_traceback = models.TextField()
 
     stack_info = models.TextField() #JSON representation of the stack, [{ 'locals' : {}, 'source': '' }]
 
+    app_version = models.CharField(max_length=64, default='Unknown')
+
     @classmethod
     def log_event(cls, request, response=None, exception=None):
+        from django.views.debug import ExceptionReporter
 
+        html_traceback = ""
         if exception:
             exc_type, exc_value, exc_traceback = sys.exc_info()
             summary = "{0}: {1}".format(exception.__class__.__name__, unicode(exception))
@@ -71,10 +79,17 @@ class Event(models.Model):
                 frame = frame.tb_next
 
             level = EVENT_LEVEL_ERROR
+
+            #Get the stacktrace.
+            try:
+                reporter = ExceptionReporter(request, is_email=False, *(exc_type, exc_value, exc_traceback))
+                html_traceback = reporter.get_traceback_html()[:1000000]
+            except:
+                logging.exception("Unable to get html traceback for some reason")
         else:
             summary = "{0} at {1}".format(response.status_code, request.path)
             lineno = 0
-            path = ""
+            path = "?".join([request.path, request.META.get('QUERY_STRING')])
             stack_info = {}
             exception = HttpResponse()
             level = EVENT_LEVEL_WARNING if response.status_code == 404 else EVENT_LEVEL_INFO
@@ -94,13 +109,16 @@ class Event(models.Model):
         def txn(_error, _stack_info):
             _error = Error.objects.get(pk=_error.pk)
 
-            event = Event.objects.create(
+            Event.objects.create(
                 error=_error,
+                request_repr=repr(request).strip(),
                 request_method=request.method,
                 request_url=request.path,
                 request_querystring=request.META['QUERY_STRING'],
                 logged_in_user_email=getattr(getattr(request, "user", None), "email", ""),
                 stack_info=json.dumps(_stack_info),
+                app_version=os.environ.get('CURRENT_VERSION_ID', 'Unknown'),
+                html_traceback=html_traceback
             )
 
             _error.last_event = timezone.now()
--- views.py
+++ views.py
@@ -1,8 +1,9 @@
-# Create your views here.
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Error
+
 
 def index(request):
     errors = Error.objects.all().order_by("-last_event")
@@ -11,4 +12,13 @@ def index(request):
 def error(request, error_id):
     error = get_object_or_404(Error, pk=error_id)
 
-    return render(request, "centaur/error.html", { "error": error })
+    page = request.GET.get('page', 0)
+    paginator = Paginator(error.events.order_by('-created'), 1)
+    try:
+        events = paginator.page(page)
+    except PageNotAnInteger:
+        events = paginator.page(1)
+    except EmptyPage:
+        events = paginator.page(paginator.num_pages)
+
+    return render(request, "centaur/error.html", { "error": error, "events": events })
