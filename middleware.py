02ed3125586e21f125d7f8ccb7c18ee65752fddc
Luke Benstead <lukeb@potatolondon.com>
2014-09-24 10:25:27 +0100

    Gracefully handle logging 4xx responses

--- middleware.py
+++ middleware.py
@@ -8,8 +8,13 @@ class CentaurMiddleware(object):
         #We log errors when the status code is greater than 400, but not 408 which is the status code
         #used by deferred tasks when they (explicitly) retry. This isn't an error, and causes contention.
         if response.status_code > 400 and response.status_code != 408 and not getattr(request, "_exception_logged", False):
-            event = Event.log_event(request, response=response)
-            request.centaur_event = event
+            try:
+                event = Event.log_event(request, response=response)
+                request.centaur_event = event
+            except:
+                #Same as below, ignore errors while logging to centaur
+                logging.exception("Unable to log the error to centaur")
+
         return response
 
     def process_exception(self, request, exception):
